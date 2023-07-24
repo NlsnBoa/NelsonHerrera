2d159ad8cfff1560e9a76c48f644b933cd19e828
Nelson Herrera <ndanielherrera@icloud.com>
2023-07-24 00:30:39 -0400

    Moved all assets to src

--- src/assets/font/ValenciaFolder/README.txt
+++
@@ -1,3 +0,0 @@
-To learn more about the font family and its license, visit https://www.fontmirror.com/valencia
-
-To learn more about the font, visit https://www.behance.net/gallery/56515511/Valencia-Calligraphy-Typeface-Free-Download.
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,13 +4,13 @@ const Contact = () => {
   return (
     <div className={styles.container}>
       <div className={styles.link}>
-        <a target='_blank' href="https://github.com/NlsnBoa"><img className={styles.linkImg} src="src/assets/SocialMedia/github_line.svg" alt="" /></a>
+        <a target='_blank' href="https://github.com/NlsnBoa"><img className={styles.linkImg} src="github_line.svg" alt="" /></a>
       </div>
       <div className={styles.link}>
-        <a target='_blank' href="https://www.linkedin.com/in/nelson-herrera-swe/"><img className={styles.linkImg} src="src/assets/SocialMedia/linkedin_fill.svg" alt="" /></a>
+        <a target='_blank' href="https://www.linkedin.com/in/nelson-herrera-swe/"><img className={styles.linkImg} src="linkedin_fill.svg" alt="" /></a>
       </div>
       <div className={styles.link}>
-        <a target='_blank' href="ndanielherrera@icloud.com"><img className={styles.linkImg} src="src/assets/SocialMedia/mail_fill.svg" alt="" /></a>
+        <a target='_blank' href="ndanielherrera@icloud.com"><img className={styles.linkImg} src="mail_fill.svg" alt="" /></a>
       </div>
 
       <div className={styles.link}>
